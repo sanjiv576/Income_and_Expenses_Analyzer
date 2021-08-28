4fae7f89ad82eecb4d163942cbeb76b6140fdf00
sanjiv576 <83968516+sanjiv576@users.noreply.github.com>
2021-08-28 18:57:23 +0545

    registration form has been updated.

---
+++ userAccount.py
@@ -0,0 +1,12 @@
+"""
+This module includes information of income and expenses of the user after login,
+which stores, manages, updates and accesses the information as the user provides.
+"""
+
+from tkinter import *
+myAccount = Tk()
+myAccount.title("MY ACCOUNT")
+myAccount.iconbitmap("login.ico")
+myAccount.geometry("800x800")
+
+myAccount.mainloop()
